cc69755bc0506a8ef926b6c40292e8d02b595ead
Marco <marco.consolidani@fincatierinxt.com>
2025-01-20 16:48:36 +0100

    Add ComponentViewModel baseclass

---
+++ src/Catalog/Architecture/Component/IComponentViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel;
+
+namespace Catalog.Architecture.Component;
+
+public interface IComponentViewModel: INotifyPropertyChanged
+{
+    Task OnInitializeAsync();
+}
---
+++ src/Catalog/Architecture/Injector/ComponentViewModelInject.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using Catalog.Architecture.Component;
+
+namespace Catalog.Architecture.Injector;
+
+public abstract class ComponentViewModelInject<TViewModel> : ComponentBase, IDisposable
+    where TViewModel : IComponentViewModel
+{
+    [Inject]
+    [NotNull]
+#pragma warning disable CS8618
+    protected TViewModel ViewModel { get; set; }
+#pragma warning restore CS8618
+
+    private void OnStateChange(object? sender, PropertyChangedEventArgs e)
+    {
+        StateHasChanged();
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        ViewModel.PropertyChanged += (_, _) => StateHasChanged();
+        await ViewModel.OnInitializeAsync();
+    }
+
+    public void Dispose()
+    {
+        ViewModel.PropertyChanged -= OnStateChange;
+    }
+}
